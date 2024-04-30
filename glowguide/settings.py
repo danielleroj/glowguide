6a43cad27ef0d31e22c730377cc8e4cc13c0666a
danielleroj <danielleroj@icloud.com>
2024-04-30 00:33:20 -0400

    Add authentication

--- glowguide/settings.py
+++ glowguide/settings.py
@@ -127,6 +127,8 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/routines/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
