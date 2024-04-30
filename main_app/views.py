1bd27b89971e9efddc2a0a8193e86db6eb1209f9
danielleroj <danielleroj@icloud.com>
2024-04-30 13:08:07 -0400

    Filter routines to only user created routines

--- main_app/views.py
+++ main_app/views.py
@@ -3,11 +3,26 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Routine, Product, Photo
 
 import boto3, uuid
 
 # Create your views here.
+def signup(request):
+    erorr_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            erorr_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_mesage': erorr_message}
+    return render(request, 'registration/signup.html', context)
 
 def home(request):
     return render(request, 'home.html')
@@ -16,7 +31,7 @@ def about(request):
     return render(request, 'about.html')
 
 def routines_index(request):
-    routines = Routine.objects.all()
+    routines = Routine.objects.filter(user=request.user)
     return render(request, 'routines/index.html', {
         'routines': routines
     })
